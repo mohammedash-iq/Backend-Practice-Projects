585b8ec42a7deceddb2c55ba936713193c11a0d5
Mohammed Ash <mohammedashiq206@gmail.com>
2025-03-16 07:07:19 +0000

    refined the code for log.js v2

--- node/log.js
+++ node/log.js
@@ -1,6 +1,6 @@
 const file = require("fs");
 const date = new Date();
-data="\n program is exwcuted on " + date;
-file.appendFile("./log.txt",data,(error)=>{
-if (error)console.log("an error coused while logging the file")})
-
+data = `\n program is exwcuted on ${date}`;
+file.appendFile("./log.txt", data, (error) => {
+  if (error) console.log("an error coused while logging the file"+error);
+});
