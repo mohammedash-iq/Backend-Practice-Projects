0f4c77274ee1f40e86a4f8ce79ea875da7fa98f9
Mohammed Ash <mohammedashiq206@gmail.com>
2025-03-16 07:05:11 +0000

    refined the code for log.js

--- node/log.js
+++ node/log.js
@@ -1,13 +1,6 @@
 const file = require("fs");
 const date = new Date();
-file.readFile("./log.txt", "utf-8", (error, data) => {
-    if(error){
-        console.log(error+"\n file cannot be read")
-    }
-  data = data + "\n program is exwcuted on " + date;
-  file.writeFile("./log.txt", data, (error) => {
-    if (error) {
-      console.log(error+"file cannot be written");
-    }
-  });
-});
+data="\n program is exwcuted on " + date;
+file.appendFile("./log.txt",data,(error)=>{
+if (error)console.log("an error coused while logging the file")})
+
